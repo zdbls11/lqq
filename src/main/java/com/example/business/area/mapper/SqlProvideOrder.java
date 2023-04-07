10f0ea77fcd05a9c18dfa820fc4f83e0fdb0d10d
刘文鹏 <lwp13522606909>
2023-04-07 10:30:12 +0800

    修正

--- src/main/java/com/example/business/area/mapper/SqlProvideOrder.java
+++ src/main/java/com/example/business/area/mapper/SqlProvideOrder.java
@@ -4,6 +4,7 @@ import com.example.business.area.dto.request.QueryOrderRequest;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 /**
  * @Author: liuwenpeng
@@ -11,35 +12,35 @@ import java.util.Date;
  */
 public class SqlProvideOrder {
 
-    public String queryOrder(QueryOrderRequest request){
+    public String queryOrder(QueryOrderRequest request) {
         Date date = new Date();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 //        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String sql =" select * from `order`" +
+        String sql = " select * from `order`" +
                 " left join area on area.id = order.area_id " +
                 " left join user on user.id = order.user_id " +
                 "where 1=1 ";
-        if(request.getArea()!=null){
-            sql+=" and area.name like '%"+request.getArea()+"%' ";
+        if (request.getArea() != null && !Objects.equals(request.getArea(), "")) {
+            sql += " and area.name like '%" + request.getArea() + "%' ";
         }
-        if(request.getUser()!=null){
-            sql+=" and user.username like '%"+request.getUser()+"%' ";
+        if (request.getUser() != null && !Objects.equals(request.getUser(), "")) {
+            sql += " and user.username like '%" + request.getUser() + "%' ";
         }
-        if(request.getTime()!=null){
-            sql+=" and order.time = '"+request.getTime()+"' ";
+        if (request.getTime() != null) {
+            sql += " and order.time = '" + request.getTime() + "' ";
         }
-        if(request.getDay()!=null){
-            sql+=" and order.order_date like '%"+request.getDay()+"%' ";
+        if (request.getDay() != null && !Objects.equals(request.getDay(), "")) {
+            sql += " and order.order_date like '%" + request.getDay() + "%' ";
         }
-        if(request.getRole()==1){
-            sql+=" and order.user_id = '"+request.getUser_id()+"' ";
+        if (request.getRole() == 1) {
+            sql += " and order.user_id = '" + request.getUser_id() + "' ";
         }
-        if(!request.getIs_history()){
-            sql+=" and `order`.order_date >= '"+sdf.format(date)+" 00:00:00' ";
-        }else {
-            sql+=" and `order`.order_date < '"+sdf.format(date)+" 00:00:00' ";
+        if (!request.getIs_history()) {
+            sql += " and `order`.order_date >= '" + sdf.format(date) + " 00:00:00' ";
+        } else {
+            sql += " and `order`.order_date < '" + sdf.format(date) + " 00:00:00' ";
         }
-        sql+= " order by `order`.order_date asc,`order`.time asc";
+        sql += " order by `order`.order_date asc,`order`.time asc";
         return sql;
     }
 }
